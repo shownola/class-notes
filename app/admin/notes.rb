54acd6abfdbe89a1cfdf2d1fee093c280e4ed80b
shownola <swasieko@gmail.com>
2021-01-07 17:04:33 -0700

    Add quill editor and additionaal styles for notes

--- app/admin/notes.rb
+++ app/admin/notes.rb
@@ -3,11 +3,32 @@ ActiveAdmin.register Note do
 
   form title: 'Notes App' do |f|
     f.inputs 'Note' do
-      f.input :subject_id
+      f.input :subject
       f.input :title
-      f.input :body
+      f.input :body, as: :quill_editor
     end
     f.actions
+  end
+
+  show title: 'Your Note' do
+    h1 link_to note.title, admin_notes_path
+    h4 link_to note.subject.name, admin_subject_path(note.subject)
+    div(class: 'note-body') do
+      raw note.body
+    end
+  end
+
+  index do
+    selectable_column
+    column :subject
+    column 'Title' do |note|
+      link_to note.title, admin_note_path(note)
+    end
+    column 'Body' do |note|
+      raw note.body.truncate_words(10)
+    end
+    column :created_at
+    column :updated_at
   end
 
   # See permitted parameters documentation:
